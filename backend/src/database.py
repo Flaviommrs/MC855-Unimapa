6f2b3da0a859cae99ec17552e66d75fb746868bf
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-03-29 12:46:48 -0300

    Add simple hello world

---
+++ backend/src/database.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+USERS = {
+    0: {
+        "name" : "Allan Nozomu Fukasawa", 
+    },
+    1: {
+        "name" : "Moana dos Santos", 
+    },
+}
--- backend/src/unimapa.py
+++ backend/src/unimapa.py
@@ -1,8 +1,42 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+from src.schemas import UserSchema
+from src.database import USERS
+from src.models import User
+
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+class UserResource(Resource):
+    def get(self, user_id):
+        return USERS[user_id], 200
+
+
+class UserListResource(Resource):
+    def get(self):
+        return USERS
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name')
+
+        args = parser.parse_args()
+        newUser = User(args['name'])
+        print(UserSchema().dump(newUser).data)
+        USERS[len(USERS)] = UserSchema().dump(newUser).data
+
+        return USERS[len(USERS) - 1], 201
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(UserListResource, '/users')
+api.add_resource(UserResource, '/users/<int:user_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
