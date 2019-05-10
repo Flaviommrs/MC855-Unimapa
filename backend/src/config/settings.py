fe41119e7d701429921e83c435d3d37b4cd0c5f3
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-05-10 20:15:09 -0300

    Add new configuration, database

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -6,3 +6,5 @@ load_dotenv()
 
 FCM_KEY = os.getenv("FCM_KEY", None)
 FCM_TEST_USER = os.getenv("FCM_TEST_USER", None)
+FIREBASE_PROJECT_ID = os.getenv("FCM_ID", None)
+DATABASE_CONFIG = os.getenv("DATABASE_CONFIG", None)
