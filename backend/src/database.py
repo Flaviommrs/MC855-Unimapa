e8464696c8133b05ee203fb7aece94ed4306e0c7
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-05-10 20:15:45 -0300

    Change database to use relational data

--- backend/src/database.py
+++ backend/src/database.py
@@ -7,36 +7,43 @@ from geojson import Point
 
 from .models import User, Map, Subscription, Post
 
-def create_database():
-    if not User.exists():
-        User.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
-    if not Map.exists():
-        Map.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
-    if not Subscription.exists():
-        Subscription.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
-    if not Post.exists():
-        Post.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
-
-def mock_database():
+def mock_database(db):
     if 'FLASK_ENV' in os.environ and os.environ['FLASK_ENV'] == 'development':
+        users = []
+        maps = []
+
         for i in range(50):
-            new_user = User("Username {}".format(i), name="Nome {}".format(i))
-            new_user.save()
+            new_user = User(
+                username="Username {}".format(i),
+                name="Nome {}".format(i),
+                email='username{}@gmail.com'.format(i)
+            )
+            users.append(new_user)
+            db.session.add(new_user)
         
         for i in range(5):
-            new_map = Map(i, name="Mapa {}".format(i))
-            new_map.save()
+            new_map = Map(name="Mapa {}".format(i))
+            maps.append(new_map)
+            db.session.add(new_user)
 
         for i in range(10):
-            new_subscription = Subscription(i % 5, "Username {}".format(i), subscription_time=datetime.utcnow())
-            new_subscription.save()
+            new_subscription = Subscription(
+                user = users[i],
+                map = maps[i % 5],
+                subscription_time = datetime.utcnow()
+            )
+            db.session.add(new_subscription)
 
-        for i in range(10):
+        for i in range(20):
             c = datetime.utcnow()
-            microseconds = (c.day * 24 * 60 * 60 + c.second) * 1000000 + c.microsecond
-            new_post = Post(i % 3, microseconds,
-                post_time=datetime.utcnow(), 
-                message="Post {}".format(i),
-                point=Point((random.uniform(-47.1,-47.0), random.uniform(-22.9,-22.8))),
-                username="Username {}".format(i))
-            new_post.save()
+            new_post = Post(
+                post_time = datetime.utcnow(), 
+                message = "Post {}".format(i),
+                point_x = random.uniform(-47.1,-47.0),
+                point_y = random.uniform(-22.9,-22.8),
+                user = users[i % 5],
+                map = maps[i % 3]
+            )
+            db.session.add(new_post)
+
+        db.session.commit()
