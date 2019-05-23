803ca606db1e727e6f85b050af4e62988bfa22d3
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-05-23 12:23:32 -0300

    Change project structure

--- backend/src/unimapa.py
+++ backend/src/unimapa.py
@@ -17,7 +17,12 @@ from . import resources
 from . import database
 from . import notifications
 
-def create_app():
+# Firebase auth configuration
+cred = credentials.Certificate('./serviceAccountKey.json')
+firebase_app = firebase_admin.initialize_app(cred)
+
+
+def create_app(test_config=None):
     app = Flask(__name__)
 
     # Database configuration
@@ -25,61 +30,65 @@ def create_app():
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
     else:
         app.config['SQLALCHEMY_DATABASE_URI'] = settings.DATABASE_CONFIG
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    if test_config:
+        app.config.update(test_config)
 
     db.init_app(app)
     migrate = Migrate(app, db)
 
-    # Firebase auth configuration
-    cred = credentials.Certificate('./serviceAccountKey.json')
-    firebase_app = firebase_admin.initialize_app(cred)
+
+    # Routes
+    api = Api(app)
+    api.add_resource(resources.SignUpResource, '/sign-up')
+
+    api.add_resource(resources.UserListResource, '/users')
+    api.add_resource(resources.UserResource, '/users/<int:user_id>')
+    api.add_resource(resources.UserSubscriptionListResource, '/users/<int:user_id>/subscriptions')
+    api.add_resource(resources.UserPostListResource, '/users/<int:user_id>/posts')
+
+    api.add_resource(resources.MapListResource, '/maps')
+    api.add_resource(resources.MapResource, '/maps/<int:map_id>')
+    api.add_resource(resources.MapSubscriptionListResource, '/maps/<int:map_id>/subscriptions')
+    api.add_resource(resources.MapPostListResource, '/maps/<int:map_id>/posts')
+
+    api.add_resource(resources.SubscriptionListResource, '/subscriptions')
+    api.add_resource(resources.SubscriptionResource, '/subscriptions/<int:subscription_id>')
+
+    api.add_resource(resources.PostListResource, '/posts')
+
+    api.add_resource(resources.TokenResource, '/token')
+
+    api.add_resource(resources.MyAccount, '/my_account')
+    api.add_resource(resources.MyMaps, '/my_subscriptions')
+
+
+    @app.cli.command()
+    def create_database():
+        database.create_database(db)
+        print("Database created")
+
+
+    @app.cli.command()
+    def mock_database():
+        session = db.create_scoped_session()
+        database.mock_database_users(session, 50)
+        database.mock_database_maps(session, 5)
+        database.mock_database_subscriptions(session, 5)
+        database.mock_database_posts(session, 20)
+        print("Database mocked")
+
+
+    @app.cli.command()
+    def send_notification():
+        notifications.send_notification([], "Hello", "World")
+        print("Notification Sent")
+
+
+    # Commands
+    app.cli.add_command(mock_database)
+    app.cli.add_command(create_database)
+    app.cli.add_command(send_notification)
 
     return app
-
-# App creation
-app = create_app()
-
-@app.cli.command()
-def create_database():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-    print("Database created")
-
-@app.cli.command()
-def mock_database():
-    database.mock_database(db)
-    print("Database mocked")
-
-@app.cli.command()
-def send_notification():
-    notifications.send_notification([], "Hello", "World")
-    print("Notification Sent")
-
-# Commands
-app.cli.add_command(mock_database)
-app.cli.add_command(create_database)
-app.cli.add_command(send_notification)
-
-# Routes
-api = Api(app)
-api.add_resource(resources.SignUpResource, '/sign-up')
-
-api.add_resource(resources.UserListResource, '/users')
-api.add_resource(resources.UserResource, '/users/<int:user_id>')
-api.add_resource(resources.UserSubscriptionListResource, '/users/<int:user_id>/subscriptions')
-api.add_resource(resources.UserPostListResource, '/users/<int:user_id>/posts')
-
-api.add_resource(resources.MapListResource, '/maps')
-api.add_resource(resources.MapResource, '/maps/<int:map_id>')
-api.add_resource(resources.MapSubscriptionListResource, '/maps/<int:map_id>/subscriptions')
-api.add_resource(resources.MapPostListResource, '/maps/<int:map_id>/posts')
-
-api.add_resource(resources.SubscriptionListResource, '/subscriptions')
-api.add_resource(resources.SubscriptionResource, '/subscriptions/<int:subscription_id>')
-
-api.add_resource(resources.PostListResource, '/posts')
-
-api.add_resource(resources.TokenResource, '/token')
-
-api.add_resource(resources.MyAccount, '/my_account')
-api.add_resource(resources.MyMaps, '/my_subscriptions')
