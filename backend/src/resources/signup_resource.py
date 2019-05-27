22a39217698c2e2021a2cfcbf2cf78aeecc67e73
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-05-26 21:36:09 -0300

    Add notification token when signup

--- backend/src/resources/signup_resource.py
+++ backend/src/resources/signup_resource.py
@@ -4,7 +4,7 @@ from flask_restful import Resource, reqparse, Api
 from .decorators import authenticate, decode_token
 
 from ..schemas import UserSchema
-from ..models import User
+from ..models import User, NotificationToken, db
 
 from ..config import settings
 
@@ -19,6 +19,21 @@ class SignUpResource(Resource):
         name = self.decoded_token['name']
         email = self.decoded_token['email']
         user = User.get_or_create(User, id=uid, name=name, email=email)
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('notification_token')
+        args = parser.parse_args()
+
+        print(args['notification_token'])
+
+        notification_token = NotificationToken(
+            notification_token = args['notification_token'],
+            user = user
+        )
+
+        db.session.add(notification_token)
+        db.session.commit()
+        
         return UserSchema().dump(user).data, 201
 
 api.add_resource(SignUpResource, '/sign-up')
