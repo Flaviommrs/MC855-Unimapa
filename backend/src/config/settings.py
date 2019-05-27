291d7a812889978f0f3ed3dab0eefc5555e98cb0
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-05-26 21:38:56 -0300

    Remove unused config const FCM_TEST_USER

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY", None)
 FCM_KEY = os.getenv("FCM_KEY", None)
-FCM_TEST_USER = os.getenv("FCM_TEST_USER", None)
 FIREBASE_PROJECT_ID = os.getenv("FCM_ID", None)
 DATABASE_CONFIG = os.getenv("DATABASE_CONFIG", None)
 DATABASE_CONFIG_LOCAL = os.getenv("DATABASE_CONFIG_LOCAL", None)
