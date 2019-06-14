afa03e16582783bb85ed137403e8a021fe6a55a6
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-06-14 15:59:36 -0300

    Add read_only maps

--- backend/src/models.py
+++ backend/src/models.py
@@ -50,10 +50,10 @@ class User(db.Model, MyModel):
     def __repr__(self):
         return '<User {}>'.format(self.email)
 
-    
 class Map(db.Model, MyModel):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    read_only = db.Column(db.Boolean, nullable=False, default=False)
 
     user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable=False)
     
