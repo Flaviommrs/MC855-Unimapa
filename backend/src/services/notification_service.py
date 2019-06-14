5e33a1a44fb29b70a65ef9e392d0c41b31a79fec
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-06-14 17:19:16 -0300

    Add notification logic to not send if in development mode

--- backend/src/services/notification_service.py
+++ backend/src/services/notification_service.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 from ..models import Map, User, NotificationToken, Post, Subscription
 from ..config import settings
@@ -7,11 +8,16 @@ from ..config import settings
 def send_notification(_map, post):
     subscriptions = _map.subscriptions
     users = [sub.user for sub in subscriptions]
-    user_tokens = [user.notification_tokens for user in users]
+    user_tokens = [user.notification_tokens for user in users if user != post.user]
     tokens = []
     for user_token in user_tokens:
         tokens += user_token
     tokens = [token.notification_token for token in tokens]
+
+    if 'FLASK_ENV' in os.environ and os.environ['FLASK_ENV'] == 'development':
+        print('Send local notification to:')
+        for token in tokens:
+            print(token)
 
     URL = 'https://fcm.googleapis.com/fcm/send'
     FCM_KEY = settings.FCM_KEY
@@ -23,5 +29,6 @@ def send_notification(_map, post):
             'body' : post.message
         } 
     }
+
     headers = { 'Authorization' : 'key={}'.format(FCM_KEY) , 'Content-type' : 'application/json'}
     r = requests.post(URL, headers=headers, data=json.dumps(data))
