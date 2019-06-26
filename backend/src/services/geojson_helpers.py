955db5441bc4e99d6629650d47263ae2d6cbdf32
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-06-25 22:48:03 -0300

    Change lat and lon, add description

--- backend/src/services/geojson_helpers.py
+++ backend/src/services/geojson_helpers.py
@@ -9,16 +9,14 @@ def posts_to_geojson(posts):
             new_feature = Feature(
                 geometry=Point((post.point_y, post.point_x)),
                 properties={
-                    'title' : post.title,
-                    'message' : post.message
+                    'description' : '<strong>{}</strong><p>{}</p>'.format(post.title, post.message),
                 }
             )
         else:
             new_feature = Feature(
                 geometry=Point((0,0)),
                 properties={
-                    'title' : post.title,
-                    'message' : post.message
+                    'description' : '<strong>{}</strong><p>{}</p>'.format(post.title, post.message),
                 }
             )
         feature_collection_list.append(new_feature)
